4a6d5ffa73121ac95f3ccbd3af8994b73f6cf2cf
Rafael Bomfim <49501710+RafaelBomfim128@users.noreply.github.com>
2025-07-24 17:40:43 -0300

    Update formatRedirects.test.js

--- __tests__/formatters/formatRedirects.test.js
+++ __tests__/formatters/formatRedirects.test.js
@@ -6,7 +6,7 @@ describe('formatRedirects', () => {
             ['xDrip', 'short-xdrip', 'https://example.com/full-url']
         ];
         const result = formatRedirects(links);
-        expect(result).toBe('short-xdrip  https://example.com/full-url  200');
+        expect(result).toBe('short-xdrip  https://example.com/full-url  301');
     });
 
     it('format multiple redirects correctly', () => {
@@ -15,7 +15,7 @@ describe('formatRedirects', () => {
             ['Android APS', 'short-aps', 'https://example.com/aps-full-url']
         ];
         const result = formatRedirects(links);
-        expect(result).toBe('short-xdrip  https://example.com/xdrip-full-url  200\nshort-aps  https://example.com/aps-full-url  200');
+        expect(result).toBe('short-xdrip  https://example.com/xdrip-full-url  301\nshort-aps  https://example.com/aps-full-url  301');
     });
 
     it('format redirect handling special characters correctly', () => {
@@ -23,7 +23,7 @@ describe('formatRedirects', () => {
             ['xDrip', ' shóÒõôrt xdrip@#$%({ ', ' https://example.com/full-url ']
         ];
         const result = formatRedirects(links);
-        expect(result).toBe('shoooort-xdrip  https://example.com/full-url  200');
+        expect(result).toBe('shoooort-xdrip  https://example.com/full-url  301');
     });
 
     it('handle empty input', () => {
@@ -41,6 +41,6 @@ describe('formatRedirects', () => {
             ['xDrip5', 'short-xdrip5', 'https://example.com/xdrip-full-url5'], //OK
         ];
         const result = formatRedirects(links);
-        expect(result).toBe('short-xdrip2  https://example.com/xdrip-full-url2  200\nshort-xdrip5  https://example.com/xdrip-full-url5  200');
+        expect(result).toBe('short-xdrip2  https://example.com/xdrip-full-url2  301\nshort-xdrip5  https://example.com/xdrip-full-url5  301');
     });
 });
