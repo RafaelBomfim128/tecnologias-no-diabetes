09cb93f195fadcaf3aa169825b1b783dafdd3c78
Rafael Assis <ra-bonfim@hotmail.com>
2026-04-29 17:45:55 -0300

    Fix QR Code Generator For Juggluco

--- public/js/gerador-qr-code-juggluco.js
+++ public/js/gerador-qr-code-juggluco.js
@@ -4,9 +4,10 @@ function gerarQRCode() {
     const qrCodeImg = document.getElementById("qrCode");
     const data1 = encodeURIComponent(input1.trim()).toUpperCase();
     const data2 = encodeURIComponent(input2.trim()).toUpperCase();
-    const additionalData = '(01)06972831641803(11)241220(11)301220(10)LT8J381085F';
+    const groupSeparator = '0' //this replaces the special character "GS" (\x1D) in the QR Code
+    const additionalData = `${groupSeparator}0106972831641803112412201130122010LT8J381085F${groupSeparator}`;
     if (input1.length === 2 && input2.length === 18) {
-        qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${additionalData}(${data1})${data2}`;
+        qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${additionalData}${data1}${data2}`;
         qrCodeImg.alt = "QR Code gerado com o texto fornecido";
     } else {
         qrCodeImg.src = "";
