c94a82c94d5dd6c7a5827f0a3fd57bf950c1bb23
Rafael Assis <ra-bonfim@hotmail.com>
2026-04-28 22:00:36 -0300

    Fix gerador de qr code

--- public/js/gerador-qr-code-juggluco.js
+++ public/js/gerador-qr-code-juggluco.js
@@ -4,7 +4,7 @@ function gerarQRCode() {
     const qrCodeImg = document.getElementById("qrCode");
     const data1 = encodeURIComponent(input1.trim()).toUpperCase();
     const data2 = encodeURIComponent(input2.trim()).toUpperCase();
-    const additionalData = '(01)06972831641803(11)241220(10)LT8J381085F';
+    const additionalData = '(01)06972831641803(11)241220(11)301220(10)LT8J381085F';
     if (input1.length === 2 && input2.length === 18) {
         qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${additionalData}(${data1})${data2}`;
         qrCodeImg.alt = "QR Code gerado com o texto fornecido";
